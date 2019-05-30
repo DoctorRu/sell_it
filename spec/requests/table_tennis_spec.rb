590b0cec81c81d90abbe5e29a6fde936abfc8e23
DoctorRu <vjblan@gmail.com>
2019-05-29 23:21:35 -0300

    RSpec: Table Tennis

--- spec/requests/table_tennis_spec.rb
+++ spec/requests/table_tennis_spec.rb
@@ -3,8 +3,14 @@ require 'rails_helper'
 RSpec.describe 'Table Tennis API', type: :request do
     describe '#ping' do
         context 'when unauthenticated' do
+
+            before { get '/ping' }    
+
+            it 'works' do
+                expect(response).to be_success
+            end
+
             it 'returns unauthenticated pong' do
-                get '/ping'
                 expect(parsed_body['response']).to eq 'Error - Unauthorized'
             end
         end
